40d15e93d3b3211ef36cc1366eea35150c7526ef
master <dimad@udm.ru>
2024-11-19 15:28:12 +0400

    1

---
+++ Grasshopper - Basic Function Fixer.py
@@ -0,0 +1,17 @@
+# Fix the function
+# I created this function to add five to any number that was passed in to it and return the new value.
+# It doesn't throw any errors but it returns the wrong number.
+#
+# Can you help me fix the function?
+
+# Исправьте функцию
+# # Я создал эту функцию, чтобы добавлять пять к любому числу, которое было передано в нее, и возвращать новое значение.
+# Она не выдает никаких ошибок, но возвращает неправильное число.
+#
+# Можете ли вы помочь мне исправить эту функцию?
+
+def add_five(num):
+    total = num + 5
+    return total
+
+print(add_five(5))
