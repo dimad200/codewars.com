dc00fdffbf3d92a8f7cd924c6a812eb054bc42bc
master <dimad@udm.ru>
2024-11-19 11:52:28 +0400

    1

--- 1.py
+++ 1.py
@@ -1,10 +1,32 @@
 class Rectangle:
-    def __new__(cls, *args, **kwargs):
-        print("Hello from __new__")
-        return super().__new__(cls)
     def __init__(self, width, height):
-        print("Hello from __init__")
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, w):
+        if w > 0:
+            self.__width = w
+        else:
+            raise ValueError
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self, h):
+        if h > 0:
+            self.__height = h
+        else:
+            raise ValueError
+    def area(self):
+        return self.__width * self.__height
 
-rect = Rectangle(10, 20)
+bublik=Rectangle(10,20)
+print(bublik.area())
+a=bublik.width
+print(a)
+bublik.width=0
+print(bublik.width)
+# print(bublik.area())
